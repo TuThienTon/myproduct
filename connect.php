df2bed353c268b9c1035b5995c454828ed46536d
TuThienTon <89011656+TuThienTon@users.noreply.github.com>
2022-12-01 15:53:18 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,10 @@
 <?php
-$host     = "localhost";
-$port     = 3306;
-$socket   = "";
-$user     = "root";
-$password = "1111";
-$dbname   = "myproduct";
+    $host = 'localhost';
+    $username = 'id19752994_root';
+    $pass = 'Devchido1234@';
+    $databasename = 'id19752994_myproduct';
 
-$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
-    or die ('Could not connect to the database server' . mysqli_connect_error());
+    $conn = mysqli_connect("$host","$username","$pass","$databasename");
+    mysqli_set_charset($conn,'utf8');
+    
 ?>
